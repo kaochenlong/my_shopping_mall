657806c5a90e1e60f83e73b286f51adf49a2431d
Eddie Kao <eddie@digik.com.tw>
2016-01-27 20:57:33 +0800

    add product feature tests

---
+++ spec/features/products_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "Products", type: :feature do
+  scenario "add a new product" do
+    visit products_path
+    click_link "New"
+    expect(current_path).to eq new_product_path
+    expect(page).to have_content "Add a new product"
+
+    fill_in "Title", with:"Ruby Product"
+    fill_in "Description", with: "Hello, Ruby"
+    fill_in "Price", with:100
+    click_button "Create Product"
+
+    expect(current_path).to eq products_path
+    expect(page).to have_content "成功新增商品!"
+  end
+end
