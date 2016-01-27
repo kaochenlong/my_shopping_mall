e70620253df68e6e3d3d5c6decb1e5d94be42701
Eddie Kao <eddie@digik.com.tw>
2016-01-27 20:36:06 +0800

    add context syntax

--- spec/models/example_user_spec.rb
+++ spec/models/example_user_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 describe ExampleUser do
-  it "can add a new example user" do
-    u = create(:my_dad)
-    expect(ExampleUser.count).to be 1
-  end
+  describe "add users" do
+    context "normal user" do
+      it "can add a new example user" do
+        u = create(:my_dad)
+        expect(ExampleUser.count).to be 1
+      end
+    end
 
-  it "can add a random user, but user will be unavailable" do
-    u = create(:random_user)
-    expect(u.is_available).to be false
-    expect(ExampleUser.count).to be 0  # is_available = false don't count
+    context "random user" do
+      it "can add a random user, but user will be unavailable" do
+        u = create(:random_user)
+        expect(u.is_available).to be false
+        expect(ExampleUser.count).to be 0  # is_available = false don't count
+      end
+    end
   end
 end
