c9489c5e21347c6415f9fec2a00102aecc322229
Eddie Kao <eddie@digik.com.tw>
2016-01-29 21:36:51 +0800

    add test for CartItem

---
+++ spec/models/cart_item_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe CartItem, type: :model do
+  describe "calculator" do
+    it "item can calculate it's total price itself"
+  end
+
+# cart item
+#* Some Special Event (e.g. 10% off on XMas), Optional.
+
+end
