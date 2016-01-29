cb0e0ea0266af64d721cbfbda9367245039e90d1
Eddie Kao <eddie@digik.com.tw>
2016-01-29 19:47:35 +0800

    pass "Add a item to cart, then the cart won't be empty." test

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,20 @@
 class Cart
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(product_id)
+    @items << product_id
+  end
+
+  def empty?
+    if @items.empty?
+      return true
+    else
+      return false
+    end
+  end
+
 end
+
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -5,17 +5,23 @@ RSpec.describe Cart, type: :model do
   describe "add or get item from cart" do
 
     context "add 1 item" do
-      it "Add a item to cart, then the cart won't be empty."
+      it "Add a item to cart, then the cart won't be empty." do
+        cart = Cart.new
+        expect(cart).to be_empty
+
+        cart.add_item(1)
+        expect(cart).not_to be_empty
+      end
     end
 
-    context "add more same item" do
-      it "Add more same items to cart, but the item count won't change"
-    end
+    #context "add more same item" do
+      #it "Add more same items to cart, but the item count won't change"
+    #end
 
-    context "get item from cart" do
-      it "After the item added to cart, you can get the item back from the cart."
+    #context "get item from cart" do
+      #it "After the item added to cart, you can get the item back from the cart."
 
-    end
+    #end
 
   end
 
