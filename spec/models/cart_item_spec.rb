4a3957c80504933c1570c7c13e948726d8949ae2
Eddie Kao <eddie@digik.com.tw>
2016-01-29 21:42:01 +0800

    pass "item can calculate it's total price itself" test

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
+  let(:cart) { Cart.new }
+
   describe "calculator" do
-    it "item can calculate it's total price itself"
+    it "item can calculate it's total price itself" do
+      p = create(:ruby_book, price: 80)
+      3.times { cart.add_item(p.id) }
+
+      expect(cart.items.first.price).to be 240
+    end
   end
 
 # cart item
