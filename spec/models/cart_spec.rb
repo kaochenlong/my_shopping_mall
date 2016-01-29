b9276bdb243a7df1c745ddf73e0bd4677626fb07
Eddie Kao <eddie@digik.com.tw>
2016-01-29 19:32:54 +0800

    add some tests for Cart model

---
+++ spec/models/cart_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Cart, type: :model do
+
+  describe "add or get item from cart" do
+
+    context "add 1 item" do
+      it "Add a item to cart, then the cart won't be empty."
+    end
+
+    context "add more same item" do
+      it "Add more same items to cart, but the item count won't change"
+    end
+
+    context "get item from cart" do
+      it "After the item added to cart, you can get the item back from the cart."
+
+    end
+
+  end
+
+#* Calculate total price of this cart.
+#* Can serialize all it's items to hash structure for further usage.
+#* Cart also rebuilt by import a hash structure.
+
+# cart item
+#* Every item can calculate it's total price itself.
+#* Some Special Event (e.g. 10% off on XMas), Optional.
+
+end
