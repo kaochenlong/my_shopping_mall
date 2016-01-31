5e01a17def8f4335ee590c45f1793d1c732bf5c6
Eddie Kao <eddie@digik.com.tw>
2016-01-31 16:11:29 +0800

    save order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,28 @@ class OrdersController < ApplicationController
   before_action :initialize_cart
 
   def create
-    # 刷卡
-    # insert order
-    # insert order_items
+    order = Order.new(order_params)
+    @cart.items.each do |item|
+      order.order_items.new(product_id: item.product.id, quantity: item.quantity)
+    end
 
-    # 清空 cart
-    session[:my_cart] = nil
+    if order.save
+      # 刷卡
+      # nonce = params[:payment_method_nonce]
+      #
+      # 清空 cart
+      #session[:my_cart] = nil
 
-    flash[:notice] = "感謝大爺!"
-    redirect_to products_path
+      flash[:notice] = "感謝大爺!"
+      redirect_to products_path
+    else
+      render action: 'carts/checkout'
+    end
+
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:name, :tel, :address, :email)
   end
 end
