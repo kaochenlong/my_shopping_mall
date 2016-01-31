d824d93cf202a9478b8f532b873838fdb29804e5
Eddie Kao <eddie@digik.com.tw>
2016-01-31 10:28:52 +0800

    change feature test for admin

---
+++ spec/features/admin/admin_products_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Admin::Products", type: :feature do
+  scenario "redirect to user sign in page if not sign in" do
+    visit admin_products_path
+    click_link "New"
+    expect(current_path).to eq new_user_session_path
+  end
+
+  scenario "add a new product if user sign in" do
+    # sign in
+    login_as(create(:user))
+
+    visit admin_products_path
+    click_link "New"
+    expect(current_path).to eq new_admin_product_path
+    expect(page).to have_content "新增商品"
+
+    fill_in "Title", with:"Ruby Product"
+    fill_in "Description", with: "Hello, Ruby"
+    fill_in "Price", with:100
+    click_button "Create Product"
+
+    expect(current_path).to eq admin_products_path
+    expect(page).to have_content "成功新增商品!"
+  end
+end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -2,27 +2,4 @@ require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
 
-  scenario "redirect to user sign in page if not sign in" do
-    visit products_path
-    click_link "New"
-    expect(current_path).to eq new_user_session_path
-  end
-
-  scenario "add a new product if user sign in" do
-    # sign in
-    login_as(create(:user))
-
-    visit products_path
-    click_link "New"
-    expect(current_path).to eq new_product_path
-    expect(page).to have_content "新增商品"
-
-    fill_in "Title", with:"Ruby Product"
-    fill_in "Description", with: "Hello, Ruby"
-    fill_in "Price", with:100
-    click_button "Create Product"
-
-    expect(current_path).to eq products_path
-    expect(page).to have_content "成功新增商品!"
-  end
 end
