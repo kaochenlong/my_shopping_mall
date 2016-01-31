a640cff7814c980cd2e7e6dd562763149a322d1b
Eddie Kao <eddie@digik.com.tw>
2016-01-31 14:09:15 +0800

    finish cart add

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,16 @@
 class CartsController < ApplicationController
 
   def add
-    flash[:notice] = "感謝您! 已加到購物車!"
+    product = Product.find_by(id: params[:id])
+    if product
+      cart = Cart.build_from_hash(session[:my_cart])
+      cart.add_item(product.id)
+      session[:my_cart] = cart.serialize
+      flash[:notice] = "感謝您! 已加到購物車!"
+    else
+      flash[:notice] = "查無此商品"
+    end
+
     redirect_to products_path
   end
 
