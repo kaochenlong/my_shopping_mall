5124fe7432b070401662da4a75a5c929d00d6b66
Eddie Kao <eddie@digik.com.tw>
2016-01-31 12:07:53 +0800

    add front-end page test

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
+  before :each do
+    5.times { create(:product) }
+  end
 
+  scenario "front-end page" do
+    visit products_path
+    first(".product_item").click_link("放到購物車")
+  end
 end
