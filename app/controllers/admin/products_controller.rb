5cd5a89741eded81501ef0754703cc8f05fa1996
Eddie Kao <eddie@digik.com.tw>
2016-01-31 11:09:08 +0800

    add navbar

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!
+  layout "admin"
 
   def index
     @products = Product.all
